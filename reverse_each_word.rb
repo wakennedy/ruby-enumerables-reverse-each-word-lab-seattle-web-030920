86224d87e45879d360cf66f66059460caa3df74c
William Kennedy <alexkenned@gmail.com>
2020-02-28 23:58:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(sentence)
   reversed = []
   tbr = sentence.split
-  tbr.collect do |tbr|
-    reversed << tbr.pop
+  tbr.each do |tbr|
+    tbr.reverse 
   end
   reversed
 end
